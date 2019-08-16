f11efba910c782df3390599d4577aa157a9879d5
Neal JIn Oh <no4259@gmail.com>
2019-08-16 02:55:27 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,38 @@
-# My Code here....
+def map_to_negativize(num)
+  num.map{|number| number * -1}
+end
+
+
+def map_to_no_change(dune)
+  dune.map{ |string| string}
+end
+
+def map_to_double(num)
+  num.map{ |number| number * 2}
+end
+
+def map_to_square(num)
+  num.map{ |number| number ** 2}
+end
+
+
+
+def reduce_to_total(array, starting_point = 0)
+  array.reduce(starting_point){|sum, n| sum + n}
+end
+
+def reduce_to_all_true(array)
+  array.reduce(0){|num, name| num || name}
+end
+
+def reduce_to_all_true(array)
+  array.reduce(0){|num, name| num && name}
+end
+
+def reduce_to_any_true(array)
+  array.reduce(){|num, name| num || name}
+end
+
+
+
+
